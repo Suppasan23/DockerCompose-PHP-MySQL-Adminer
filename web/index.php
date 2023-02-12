76e5afaf56d1cd73e6919d74fb3db3e5bbcdc8ec
Suppasan Chaianantakul <117501338+Suppasan23@users.noreply.github.com>
2023-02-12 14:43:35 +0700

    2:43pm 12/2/23

---
+++ web/index.php
@@ -0,0 +1,38 @@
+<link href="style.css" rel="stylesheet" type="text/css">
+
+<h2>Welcome to my Zoo hoo woo</h2>
+
+<img src="zoo.jpg" alt="Zoo" width="500px" hight="300px">
+
+    <?php
+    $mysql = new mysqli("db","root","root","zoo");
+    $sql = 'SELECT * FROM mammal';
+
+    if ($result = $mysql->query($sql))
+    {
+        while ($data = $result->fetch_object())
+        {
+            $mammal[] = $data;
+        }
+    }
+    ?>
+
+<table>
+<tr>
+    <th>id</th>
+    <th>Animals</th>
+    <th>Food</th>
+    <th>Amount</th>
+</tr>
+    <?php
+    for($i=0; $i < count($mammal); $i++)
+    {
+        echo "<tr>";
+        echo "<td>".($i+1)."</td>";
+        echo "<td>".$mammal[$i]->animal."</td>";
+        echo "<td>".$mammal[$i]->food."</td>";
+        echo "<td>".$mammal[$i]->amount."</td>";
+        echo "</tr>";
+    }
+    ?>
+</table>
